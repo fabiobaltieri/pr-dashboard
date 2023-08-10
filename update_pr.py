fb01fd8aa32480f833834da479138d9d93a7f7a1
Fabio Baltieri <fabiobaltieri@google.com>
2023-08-10 10:42:12 +0000

    use a dataclass and move some stuff for no good reasons

--- update_pr.py
+++ update_pr.py
@@ -5,6 +5,8 @@ import os
 import requests
 import time
 
+from dataclasses import dataclass
+
 token = os.environ['GITHUB_TOKEN']
 
 ORG="zephyrproject-rtos"
@@ -19,22 +21,6 @@ headers = {
         'Authorization': f'Bearer {token}',
         'Accept': 'application/vnd.github+json',
         }
-
-page = 1
-
-print(f"Loading previous data from {PR_FILE}")
-
-try:
-    with open(PR_FILE, 'r') as infile:
-        old_prs = json.load(infile)
-        print(f"Old data loaded: {len(old_prs)} PRs")
-except Exception as e:
-    print(f"Cannot load {PR_FILE}: {e}, starting from empty")
-    old_prs = {}
-
-print(f"Fetching all open PR summary")
-
-new_prs = {}
 
 def fetch_prs():
     url = f"{pr_list_url}&page={page}"
@@ -55,6 +41,27 @@ def fetch_reviews(number):
 
     return reviews
 
+@dataclass
+class Stats:
+    new: int = 0
+    cached: int = 0
+    updated: int = 0
+
+print(f"Loading previous data from {PR_FILE}")
+
+try:
+    with open(PR_FILE, 'r') as infile:
+        old_prs = json.load(infile)
+        print(f"Old data loaded: {len(old_prs)} PRs")
+except Exception as e:
+    print(f"Cannot load {PR_FILE}: {e}, starting from empty")
+    old_prs = {}
+
+print(f"Fetching all open PR summary")
+
+new_prs = {}
+
+page = 1
 while True:
     prs = fetch_prs()
     for pr in prs:
@@ -68,12 +75,12 @@ while True:
 
 print(f"Found {len(new_prs)} open PRs, updating PR details")
 
-stats = {'new': 0, 'cached': 0, 'updated': 0}
+stats = Stats()
 
 for number, data in new_prs.items():
     if not str(number) in old_prs:
         #print(f"new {number}");
-        stats['new'] += 1
+        stats.new += 1
         reviews = fetch_reviews(number)
         new_prs[number]['reviews'] = reviews
         continue
@@ -84,12 +91,12 @@ for number, data in new_prs.items():
     old_updated_at = old_data['pr']['updated_at']
     if new_updated_at == old_updated_at:
         #print(f"cache {number}");
-        stats['cached'] += 1
+        stats.cached += 1
         new_prs[number]['reviews'] = old_data['reviews']
         continue
 
     #print(f"update {number}");
-    stats['updated'] += 1
+    stats.updated += 1
     reviews = fetch_reviews(number)
     new_prs[number]['reviews'] = reviews
 
