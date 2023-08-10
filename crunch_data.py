da7282e1e5b1541f8feff6d153fd8ce7059cff34
Fabio Baltieri <fabiobaltieri@google.com>
2023-08-10 11:33:10 +0000

    add base and names

--- crunch_data.py
+++ crunch_data.py
@@ -19,7 +19,8 @@ for number, data in pr_data.items():
     author = pr['user']['login']
     users[author]['author'].append(number)
 
-    assignee_names = []
+    assignee_names = set([])
+    reviewer_names = set([])
     assignee_approved = 0
     approved = 0
     blocked = 0
@@ -27,11 +28,12 @@ for number, data in pr_data.items():
     for assignee in pr['assignees']:
         assignee_name = assignee['login']
         users[assignee_name]['assignee'].append(number)
-        assignee_names.append(assignee_name)
+        assignee_names.add(assignee_name)
 
     for reviewer in pr['requested_reviewers']:
         reviewer_name = reviewer['login']
         users[reviewer_name]['reviewer'].append(number)
+        reviewer_names.add(reviewer_name)
 
     final_review = defaultdict(str)
     for review in reviews:
@@ -44,12 +46,20 @@ for number, data in pr_data.items():
             users[reviewer_name]['approved'].append(number)
             if reviewer_name in assignee_names:
                 assignee_approved += 1
+                assignee_names.remove(reviewer_name)
+                assignee_names.add(f"+{reviewer_name}")
             else:
                 approved += 1
+                reviewer_names.add(f"+{reviewer_name}")
         elif state == 'COMMENTED':
             users[reviewer_name]['commented'].append(number)
         elif state == 'CHANGES_REQUESTED':
             users[reviewer_name]['blocking'].append(number)
+            if reviewer_name in assignee_names:
+                assignee_names.remove(reviewer_name)
+                assignee_names.add(f"-{reviewer_name}")
+            else:
+                reviewer_names.add(f"-{reviewer_name}")
             blocked += 1
         elif state == 'DISMISSED':
             users[reviewer_name]['dismissed'].append(number)
@@ -58,6 +68,9 @@ for number, data in pr_data.items():
 
     prs[number]['title'] = pr['title']
     prs[number]['author'] = author
+    prs[number]['base'] = pr['base']['ref']
+    prs[number]['assignee_names'] = assignee_names
+    prs[number]['reviewer_names'] = reviewer_names
     prs[number]['assignee_approved'] = assignee_approved
     prs[number]['approved'] = approved
     prs[number]['blocked'] = blocked
@@ -76,7 +89,7 @@ for user, data in users.items():
 
 print("")
 for pr, data in prs.items():
-    print("PR %d %d %d %d %s %s %s" % (
+    print("PR %d %d %d %d %s %s %s %s %s %s" % (
         pr,
         data['assignee_approved'],
         data['approved'],
@@ -84,4 +97,7 @@ for pr, data in prs.items():
         data['title'],
         data['updated_at'],
         data['author'],
+        data['base'],
+        data['assignee_names'],
+        data['reviewer_names'],
         ))
