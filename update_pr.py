e2687e1b9fd495dcbdff44bf7b6202ee6f2f7949
Fabio Baltieri <fabiobaltieri@google.com>
2023-08-18 14:06:42 +0000

    add argument parsing

--- update_pr.py
+++ update_pr.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import argparse
 import json
 import os
 import sys
@@ -9,13 +10,9 @@ from dataclasses import dataclass
 
 token = os.environ["GITHUB_TOKEN"]
 
-ORG = "zephyrproject-rtos"
-REPO = "zephyr"
 PER_PAGE = 100
 DATA_FILE = "cache/data_dump.json"
 BOOTSTRAP_LIMIT = 400
-
-pr_list_query = f"is:pr is:open repo:{ORG}/{REPO} repo:{ORG}/segger"
 
 @dataclass
 class Stats:
@@ -23,8 +20,8 @@ class Stats:
     cached: int = 0
     updated: int = 0
 
-def print_rate_limit(gh):
-    response = gh.get_organization(ORG)
+def print_rate_limit(gh, org):
+    response = gh.get_organization(org)
     for header, value in response.raw_headers.items():
         if header.startswith("x-ratelimit"):
             print(f"{header}: {value}")
@@ -32,9 +29,15 @@ def print_rate_limit(gh):
 def repo_from_url(url):
     return url.split('/')[-3]
 
-def fetch_pr_issues(gh):
+def fetch_pr_issues(gh, org, repos):
+    query = "is:pr is:open"
+    for repo in repos:
+        query += f" repo:{org}/{repo}"
+
+    print(f"Query: {query}")
+
     pr_issues = {}
-    issues = gh.search_issues(query=pr_list_query)
+    issues = gh.search_issues(query=query)
     for issue in issues:
         repo = repo_from_url(issue.url)
         key = f"{repo}/{issue.number}"
@@ -73,14 +76,26 @@ def update_entry(entry, pr_issue):
     entry["pr"] = pr.raw_data
     entry["reviews"] = fetch_reviews(pr)
 
+def parse_args(argv):
+    parser = argparse.ArgumentParser(description=__doc__)
+
+    parser.add_argument("-o", "--org", default="zephyrproject-rtos",
+                        help="Github organisation")
+    parser.add_argument("-r", "--repos", default="zephyr,segger",
+                        help="Github repositories, comma separated")
+
+    return parser.parse_args(argv)
+
 def main(argv):
+    args = parse_args(argv)
+
     token = os.environ.get('GITHUB_TOKEN', None)
     gh = Github(token, per_page=PER_PAGE)
 
-    print_rate_limit(gh)
+    print_rate_limit(gh, args.org)
 
     old_data = load_old_prs()
-    pr_issues = fetch_pr_issues(gh)
+    pr_issues = fetch_pr_issues(gh, args.org, args.repos.split(","))
 
     new_data = {}
     stats = Stats()
@@ -113,7 +128,7 @@ def main(argv):
         update_entry(new_data[key], pr_issue)
 
     print(stats)
-    print_rate_limit(gh)
+    print_rate_limit(gh, args.org)
 
     save_new_prs(new_data)
 
